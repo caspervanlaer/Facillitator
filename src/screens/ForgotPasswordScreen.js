54e98ba9fca24929a1b2dc548665794fe80a1775
caspervanlaer <casper@savana.solutions>
2021-05-27 11:59:57 +0200

    Update ForgotPasswordScreen.js

--- src/screens/ForgotPasswordScreen.js
+++ src/screens/ForgotPasswordScreen.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import Header from '../components/Header';
 import Spacer from '../components/Spacer';
 
 const ForgotPasswordScreen = () => {
     return <View style={stylez.view}>
         <Header name='Wachtwoord vergeten'/>
-        <Text>hallo</Text>
+        <View style={stylez.view2}>
+            <Spacer>
+                <TextInput style={stylez.input} placeholder='email adress' placeholderTextColor='green'/>
+            </Spacer>
+            <TouchableOpacity style={stylez.Container}>
+                <Text style={stylez.white}>Stuur code</Text>
+            </TouchableOpacity>
+            <Spacer>
+                <View style={stylez.textBorder}>
+                    <Text style={stylez.green}>Uw code word naar uw email gestuurd, met die code kunt u uw wachtwoord veranderen</Text>
+                </View>
+            </Spacer>
+        </View>
     </View>
 };
 
@@ -15,6 +27,40 @@ const stylez = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'white',
         height: '100%',
+    },
+    view2:{
+        height: '50%',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    input:{
+        height: 50,
+        width: 300,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingLeft: 7
+    },
+    Container:{
+        backgroundColor: 'darkgreen',
+        padding:15,
+        borderRadius: 5,
+        width: 150,
+        alignItems: 'center',
+        margin: 5
+    },
+    white:{
+        color: 'white'
+    },
+    textBorder:{
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        width: 200,
+        padding: 8
+    },
+    green:{
+        color: 'darkgreen'
     }
 });
 
