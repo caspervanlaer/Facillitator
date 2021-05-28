a465064bcbea326aad36143d1a088cd80c98110e
caspervanlaer <casper@savana.solutions>
2021-05-28 14:50:19 +0200

    Update ForgotPasswordScreen.js
    when u press send code it will show the next screen

--- src/screens/ForgotPasswordScreen.js
+++ src/screens/ForgotPasswordScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import Header from '../components/Header';
 import Spacer from '../components/Spacer';
@@ -6,17 +6,22 @@ import { Context as LanguageContext } from '../context/LanguageContext';
 
 const ForgotPasswordScreen = ({navigation}) => {
     const { state } = useContext(LanguageContext)
+    const [ sendCode, setSendCode ] = useState(false)
 
     const DutchText = 'Uw code word naar uw email gestuurd, met die code kunt u uw wachtwoord veranderen';
     const EnglishText = 'Your code will be sent to your email, with that code you can change your password';
 
-    return <View style={stylez.view}>
+    const DutchTextSend = 'Uw code is gestuurd naar uw email, Als u uw code niet heeft gekregen klik dan op de knop opnieuw verzenden';
+    const EnglishTextSend = 'Your code has been sent to your email, if you have not received your code please click the button called resend code';
+
+    if (sendCode == false) {
+        return <View style={stylez.view}>
         <Header name={state.currentLanguage == 'Nederlands'? 'Wachtwoord vergeten' : 'Password forgoten' }/>
         <View style={stylez.view2}>
             <Spacer>
                 <TextInput style={stylez.input} placeholder='email adress' placeholderTextColor='green'/>
             </Spacer>
-            <TouchableOpacity style={stylez.Container}>
+            <TouchableOpacity style={stylez.Container} onPress={() => setSendCode(true)}>
                 <Text style={stylez.white}>{state.currentLanguage == 'Nederlands'? 'Stuur code' : 'Send code' }</Text>
             </TouchableOpacity>
             <Spacer>
@@ -29,6 +34,32 @@ const ForgotPasswordScreen = ({navigation}) => {
             </View>
         </View>
     </View>
+    } else {
+        return <View style={stylez.view}>
+            <Header name={state.currentLanguage == 'Nederlands'? 'Wachtwoord vergeten' : 'Password forgoten' }/>
+            <View style={stylez.view2}>
+                <Spacer>
+                    <View style={stylez.textBorder}>
+                        <Text style={stylez.green}>{state.currentLanguage == 'Nederlands'? DutchTextSend : EnglishTextSend }</Text>
+                    </View>
+                </Spacer>
+                <Spacer>
+                    <TextInput style={stylez.input} placeholder='Code' placeholderTextColor='green'/>
+                </Spacer>
+                <Spacer>
+                    <TouchableOpacity style={stylez.Container} onPress={() => setSendCode(true)}>
+                        <Text style={stylez.white}>{state.currentLanguage == 'Nederlands'? 'Bevestig de code' : 'Confirm code' }</Text>
+                    </TouchableOpacity>
+                </Spacer>
+                <Spacer>
+                    <TouchableOpacity style={stylez.Container} onPress={() => setSendCode(true)}>
+                        <Text style={stylez.white}>{state.currentLanguage == 'Nederlands'? 'Opnieuw versturen' : 'Resend code' }</Text>
+                    </TouchableOpacity>
+                </Spacer>
+            </View>
+        </View>
+    }
+    
 };
 
 const stylez = StyleSheet.create({
